0bc63ad2d2a205b5cf82ebfe41c0119c473e343b
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2023-10-15 00:15:06 +0100

    removing bankAccount
    removing bankAccount from ext_tp1 to root folder

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,4 @@
-package ex1_tp1;
-
 public class BankAccount {
-
     private int accountNumber;
     private String accountHolderName;
     private int balance;
@@ -9,6 +6,10 @@ public class BankAccount {
     public BankAccount(int acc, String name, int bal) {
         if (bal < 0)
             throw new Error("Balance <0", null);
+        if (acc <= 0)
+            throw new Error("Account number can't be negative or 0", null);
+        if (name == "")
+            throw new Error("Name can't be empty", null);
         this.accountNumber = acc;
         this.accountHolderName = name;
         this.balance = bal;
@@ -40,8 +41,13 @@ public class BankAccount {
 
     public static void main(String[] args) {
         BankAccount account = new BankAccount(555, "John Doe", 1000);
+       // BankAccount account2 = new BankAccount(0, "az", 5);
+       // BankAccount account3 = new BankAccount(98, "", 5);
+       // BankAccount account4 = new BankAccount(7, "az", -5);
         account.deposit(100);
+       // account.deposit(-8);
         account.withdraw(50);
+       // account.withdraw(9999999);
         System.out.println(account.getAccountHolderName());
         System.out.println(account.getAccountNumber());
         System.out.println(account.getBalance());
